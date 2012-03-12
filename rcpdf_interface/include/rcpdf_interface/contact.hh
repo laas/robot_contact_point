1f9ab3a48fb2fdca9997a0009516cf639ddb8ad5
Thomas Moulard <thomas.moulard@gmail.com>
2012-03-13 00:03:46 +0100

    [rcpdf_interface] Document interfaces.

--- rcpdf_interface/include/rcpdf_interface/contact.hh
+++ rcpdf_interface/include/rcpdf_interface/contact.hh
@@ -9,9 +9,15 @@
 
 namespace rcpdf_interface
 {
+  /// \brief Geometry node imported from the URDF package.
   typedef ::urdf::Geometry Geometry;
+  /// \brief Pose node imported from the URDF package.
   typedef ::urdf::Pose Pose;
 
+  /// \brief Limitations to consider when this contact is active.
+  ///
+  /// The only limit for now is the maximum normal force which is
+  /// allowed when the robot is sustained by this contact.
   class Limit
   {
   public:
@@ -21,9 +27,20 @@ namespace rcpdf_interface
     ~Limit()
     {}
 
+    /// \brief Maximum allowed normal force.
+    ///
+    /// The contact stability and/or the robot integrity is not
+    /// guaranteed if a larger force is applied.
     double normal_force_;
   };
 
+  /// \brief Define a contact zone on the robot body.
+  ///
+  /// A contact zone is a zone which can sustain a reaction force from
+  /// the environment.
+  ///
+  /// For instance, the robot can, at least partially, be sustained by
+  /// lying on this part of its body.
   class Contact
   {
   public:
@@ -37,12 +54,32 @@ namespace rcpdf_interface
     ~Contact()
     {}
 
+    /// \brief Contact name.
+    ///
+    /// Must be unique among all robot contacts.
+    /// It is used to identify a particular contact.
     std::string name_;
+
+    /// \brief Link on which the contact is attached.
+    ///
+    /// This link must exist in the associated robot description.
+    /// This package do not require a robot description.
     std::string link_;
 
+    /// \brief Contact zone origin with respect to the link origin
+    /// frame.
+    ///
+    /// See URDF documentation for more information.
     Pose origin_;
 
+    /// \brief Geometry of the contact zone.
+    ///
+    /// See documentation of the geometry node in the URDF
+    /// documentation.
     boost::shared_ptr<Geometry> geometry_;
+    /// \brief Optional contact limitations.
+    ///
+    /// Define under which conditions this contact can be used safely.
     boost::shared_ptr<Limit> limit_;
 
     void clear()
--- rcpdf_interface/include/rcpdf_interface/model.hh
+++ rcpdf_interface/include/rcpdf_interface/model.hh
@@ -8,6 +8,10 @@
 
 namespace rcpdf_interface
 {
+  /// \brief Model of the robot contact zones.
+  ///
+  /// The model is composed of the robot name
+  /// and of a list of contacts.
   class ModelInterface
   {
   public:
@@ -18,7 +22,13 @@ namespace rcpdf_interface
     ~ ModelInterface ()
     {}
 
+    /// \brief Robot name.
+    ///
+    /// This identify the robot uniquely in order to link this model
+    /// with the URDF description.
     std::string name_;
+
+    /// \brief List of allowed contact zones.
     std::vector<boost::shared_ptr<Contact> > contacts_;
   };
 } // end of namespace rcpdf_interface.
