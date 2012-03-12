ea4db3a8dbb4eff90f507e3af17a6d879aeb4df0
Thomas Moulard <thomas.moulard@gmail.com>
2012-03-13 00:04:35 +0100

    [rcpdf] Document.

--- rcpdf/include/rcpdf/parser.hh
+++ rcpdf/include/rcpdf/parser.hh
@@ -4,6 +4,8 @@
 
 namespace rcpdf
 {
+  /// \brief Parse a RCPDF document and return a rcpdf_interface
+  /// model.
   boost::shared_ptr<rcpdf_interface::ModelInterface>
   parseRCPDF(const std::string& xml_string);
 } // end of namespace rcpdf.
